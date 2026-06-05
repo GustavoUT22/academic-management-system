ecc44590ec92ad46340b9b6b0578c178f33ce5a9
GustavoUT22 <gustavougartetorres@gmail.com>
2026-06-05 16:02:40 -0500

    add: get and update course

--- backend-express/src/services/course.service.js
+++ backend-express/src/services/course.service.js
@@ -1,8 +1,8 @@
-import mongoose from 'mongoose'
-import { Course } from '../models/course.model.js';
-import { modalities } from '../models/course.model.js';
+import mongoose from "mongoose";
+import { Course } from "../models/course.model.js";
+import { modalities } from "../models/course.model.js";
 
-const createCourseAsync = async ({
+export const createCourseAsync = async ({
   name,
   description,
   category,
@@ -12,7 +12,7 @@ const createCourseAsync = async ({
 }) => {
   const teacherExists = await User.findById(teacherId);
   if (!teacherExists) {
-    throw new Error('El docente asignado no existe');
+    throw new Error("El docente asignado no existe");
   }
   const newCourse = new Course({
     name,
@@ -20,8 +20,26 @@ const createCourseAsync = async ({
     category,
     teacherId,
     schedule,
-    modality: modalities.includes(modality) ? modality : 'virtual',
-  })
+    modality: modalities.includes(modality) ? modality : "virtual",
+  });
   await newCourse.save();
   return newCourse;
 };
+
+export const updateCourseAsync = async (courseId, updateData) => {
+  console.log("Actualizando curso con ID:", courseId);
+  const courseExists = await Course.findById(courseId);
+  if (!courseExists) {
+    throw new Error("Curso no encontrado");
+  }
+
+const updated = await Course
+
+  console.log("Curso actualizado:", updatedCourse);
+  return updatedCourse;
+};
+
+
+export const getCoursesAsync = async () => {
+  return await Course.find();
+}
